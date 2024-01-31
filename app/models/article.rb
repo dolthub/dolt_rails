be2d1ec0bb53f08e7cd24dbd093b06189577a876
Tim Sehn <tim@dolthub.com>
2024-01-31 11:09:47 -0800

    Working Dolt commits

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,11 @@ class Article < ApplicationRecord
   
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  after_commit :dolt_commit
+
+  private
+  def dolt_commit
+    ActiveRecord::Base.connection.execute("CALL DOLT_COMMIT('-am', 'Changed an Article')")
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ApplicationRecord
   include Visible
   
   belongs_to :article
+
+  after_commit :dolt_commit
+
+  private
+  def dolt_commit
+    ActiveRecord::Base.connection.execute("CALL DOLT_COMMIT('-am', 'Changed a Comment')")
+  end
 end
